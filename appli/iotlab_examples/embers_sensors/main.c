cc6023b1bfce0a7b1e05dde3fc2abca6d987795b
Gaëtan Harter <gaetan.harter@inria.fr>
2016-06-27 17:15:26 +0200

    embers_sensors: Add a 'parking' random event
    Add a parking event that randomly changes with a poisson distribution.

--- appli/iotlab_examples/embers_sensors/main.c
+++ appli/iotlab_examples/embers_sensors/main.c
@@ -3,12 +3,14 @@
 #include <printf.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include "scanf.h"
 #include "random.h"
 
 #include <platform.h>
 #include "shell.h"
 #include "soft_timer.h"
+#include "iotlab_leds.h"
 
 #include "lps331ap.h"
 #include "isl29020.h"
@@ -24,11 +26,16 @@ struct custom_timer
 // 5 seconds
 #define TIMER_DEFAULT_DELAY_SECONDS    5
 static int sensors_handler(handler_arg_t arg);
+#define PARKING_DEFAULT_DELAY_SECONDS 30
+static int parking_busy;
+static int parking_handler(handler_arg_t arg);
 
 
 static int has_random_at_start = 1;
 static struct custom_timer sensors_timer = {
     .active = 0, .handler = sensors_handler};
+static struct custom_timer parking_timer = {
+    .active = 0, .handler = parking_handler};
 
 
 static void timer_handler(handler_arg_t arg)
@@ -64,6 +71,39 @@ static int sensors_handler(handler_arg_t arg)
     return soft_timer_s_to_ticks(timer->param_delay);
 }
 
+
+unsigned poisson_step_ticks(unsigned int inv_lambda)
+{
+    double rand = random_rand16() / ((float)(1<<16));
+    double delay;
+
+    delay = -log(rand) * inv_lambda;
+    // seconds to ticks for floats
+    delay *= SOFT_TIMER_FREQUENCY;  // in seconds
+
+    return delay;
+}
+
+static void set_parking_led()
+{
+    if (parking_busy)
+        leds_on(GREEN_LED);
+    else
+        leds_off(GREEN_LED);
+}
+
+static int parking_handler(handler_arg_t arg)
+{
+    struct custom_timer *timer = (struct custom_timer *)arg;
+
+    parking_busy = !parking_busy;
+    set_parking_led();
+    printf("{\"parking\":%d}\n", (parking_busy ? 1: 0));
+
+    return poisson_step_ticks(timer->param_delay);
+}
+
+
 static unsigned random_delay_if_on(unsigned max_seconds)
 {
     if (!has_random_at_start)
@@ -92,6 +132,39 @@ static int sensors_on(int argc, char **argv)
     return 0;
 }
 
+
+static int sensors_off(int argc, char **argv)
+{
+    sensors_timer.active = 0;
+    return 0;
+}
+
+
+static int parking_on(int argc, char **argv)
+{
+    uint16_t delay = PARKING_DEFAULT_DELAY_SECONDS;
+    if (argc == 2) {
+        if (1 != sscanf(argv[1], "%u", &delay))
+            return 1;
+        if (delay == 0)
+            return 1;
+    }
+
+    parking_timer.param_delay = delay;
+    parking_timer.active = 1;
+
+    unsigned first = random_delay_if_on(parking_timer.param_delay);
+    soft_timer_start(&parking_timer.timer, first, 0);
+
+    return 0;
+}
+
+static int parking_off(int argc, char **argv)
+{
+    parking_timer.active = 0;
+    return 0;
+}
+
 static int random_on(int argc, char **argv)
 {
     has_random_at_start = 1;
@@ -100,12 +173,6 @@ static int random_on(int argc, char **argv)
 static int random_off(int argc, char **argv)
 {
     has_random_at_start = 0;
-    return 0;
-}
-
-static int sensors_off(int argc, char **argv)
-{
-    sensors_timer.active = 0;
     return 0;
 }
 
@@ -125,6 +192,9 @@ static void hardware_init()
     // LPS331AP pressure sensor initialisation
     lps331ap_powerdown();
     lps331ap_set_datarate(LPS331AP_P_12_5HZ_T_12_5HZ);
+
+    parking_busy = random_rand32() % 1;
+    set_parking_led();
 }
 
 static void timer_init()
@@ -132,11 +202,14 @@ static void timer_init()
     event_init();
     soft_timer_init();
     soft_timer_set_handler(&sensors_timer.timer, timer_handler, &sensors_timer);
+    soft_timer_set_handler(&parking_timer.timer, timer_handler, &parking_timer);
 }
 
 struct shell_command commands[] = {
     {"sensors_on",          "[delay:seconds] Start sensors measure. Default 5s, min 1s", sensors_on},
     {"sensors_off",         "Stop sensors measure",              sensors_off},
+    {"parking_on",          "[average delay:seconds] Start parking simulator. Default 30s, min 1s", parking_on},
+    {"parking_off",         "Stop parking simulator",              parking_off},
     {"random_on",           "Add a random delay before starting measures, default ON", random_on},
     {"random_off",          "No random delay before starting measures.",               random_off},
     {NULL, NULL, NULL},
