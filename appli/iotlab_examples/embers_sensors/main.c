e2fa7217e3df8f923e8f55d0b81248ff3270fda4
Gaëtan Harter <gaetan.harter@inria.fr>
2016-06-27 16:25:23 +0200

    embers_sensors: wrap timers to be non periodic
    Use non periodic timers with configurable delay until next handler.
    Allow the function to dynamically calculate delay and return it.

--- appli/iotlab_examples/embers_sensors/main.c
+++ appli/iotlab_examples/embers_sensors/main.c
@@ -9,47 +9,81 @@
 #include "shell.h"
 #include "soft_timer.h"
 
-#ifdef IOTLAB_M3
 #include "lps331ap.h"
 #include "isl29020.h"
-#endif
 
-static soft_timer_t sensors_timer;
+struct custom_timer
+{
+    soft_timer_t timer;
+    int active;
+    unsigned int param_delay;
+    int (*handler)(handler_arg_t);
+};
 
 // 5 seconds
-#define TIMER_DEFAULT_DELAY    5000
+#define TIMER_DEFAULT_DELAY_SECONDS    5
+static int sensors_handler(handler_arg_t arg);
 
-#ifdef IOTLAB_M3
+
+static struct custom_timer sensors_timer = {
+    .active = 0, .handler = sensors_handler};
+
+
+static void timer_handler(handler_arg_t arg)
+{
+    struct custom_timer *timer = (struct custom_timer *)arg;
+    if (!timer->active)
+        return;
+
+    uint32_t next = timer->handler(timer);
+    soft_timer_start(&timer->timer, next, 0);
+}
+
+
 /**
  * Sensors
  */
-static void print_sensors(handler_arg_t arg)
+static int sensors_handler(handler_arg_t arg)
 {
+    struct custom_timer *timer = (struct custom_timer *)arg;
+
     int16_t temp;
     lps331ap_read_temp(&temp);
     float lum = isl29020_read_sample();
     uint32_t pres;
     lps331ap_read_pres(&pres);
-    printf("{\"temperature\":%f,\"luminosity\":%f, \"pressure\":%f\"}\n", 42.5 + temp / 480.0, lum, pres / 4096.0 );
-    //printf("Chip temperature measure: %f\n", 42.5 + temp / 480.0);
-    //printf("Luminosity measure: %f lux\n", light);
-    //printf("Pressure measure: %f mabar\n", pres / 4096.0);
+
+    printf("{");
+    printf("\"temperature\":%f,", 42.5 + temp / 480.0);
+    printf("\"luminosity\":%f,", lum);
+    printf("\"pressure\":%f\"", pres / 4096.0 ); // No comma at the end
+    printf("}\n");
+
+    return soft_timer_s_to_ticks(timer->param_delay);
 }
-#endif
 
 static int sensors_on(int argc, char **argv)
 {
-    uint16_t delay = TIMER_DEFAULT_DELAY;
-    if (argc == 2)
+    uint16_t delay = TIMER_DEFAULT_DELAY_SECONDS;
+    if (argc == 2) {
         if (1 != sscanf(argv[1], "%u", &delay))
             return 1;
-    soft_timer_start(&sensors_timer, soft_timer_ms_to_ticks(delay), 1);
+        if (delay == 0)
+            return 1;
+    }
+
+    sensors_timer.param_delay = delay;
+    sensors_timer.active = 1;
+
+    unsigned first = 0;
+    soft_timer_start(&sensors_timer.timer, first, 0);
+
     return 0;
 }
 
 static int sensors_off(int argc, char **argv)
 {
-    soft_timer_stop(&sensors_timer);
+    sensors_timer.active = 0;
     return 0;
 }
 
@@ -57,14 +91,10 @@ static void hardware_init()
 {
     // Openlab platform init
     platform_init();
-    event_init();
-    soft_timer_init();
-    soft_timer_set_handler(&sensors_timer, print_sensors, NULL);
 
     // Switch off the LEDs
     leds_off(LED_0 | LED_1 | LED_2);
 
-#ifdef IOTLAB_M3
     // ISL29020 light sensor initialisation
     isl29020_prepare(ISL29020_LIGHT__AMBIENT, ISL29020_RESOLUTION__16bit,
             ISL29020_RANGE__16000lux);
@@ -73,12 +103,17 @@ static void hardware_init()
     // LPS331AP pressure sensor initialisation
     lps331ap_powerdown();
     lps331ap_set_datarate(LPS331AP_P_12_5HZ_T_12_5HZ);
-#endif
+}
 
+static void timer_init()
+{
+    event_init();
+    soft_timer_init();
+    soft_timer_set_handler(&sensors_timer.timer, timer_handler, &sensors_timer);
 }
 
 struct shell_command commands[] = {
-    {"sensors_on",          "Start sensors measure",              sensors_on},
+    {"sensors_on",          "[delay:seconds] Start sensors measure. Default 5s, min 1s", sensors_on},
     {"sensors_off",         "Stop sensors measure",              sensors_off},
     {NULL, NULL, NULL},
 };
@@ -87,6 +122,7 @@ int main()
 {
     hardware_init();
     shell_init(commands, 0);
+    timer_init();
     platform_run();
     return 0;
 }
