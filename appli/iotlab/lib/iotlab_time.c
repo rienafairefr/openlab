eaa95897137bc44b30fff11494a7a9b24a5f80a9
moctar ba <mocba@inrialpes.fr>
2016-08-29 16:34:40 +0200

    iotlab_time: rm get_absolute_time static function

--- appli/iotlab/lib/iotlab_time.c
+++ appli/iotlab/lib/iotlab_time.c
@@ -7,8 +7,6 @@ static inline uint32_t get_microseconds(uint64_t timer_tick, uint32_t frequency)
 static inline uint32_t get_seconds(uint64_t timer_tick, uint32_t frequency);
 static inline void ticks_conversion(struct soft_timer_timeval* time,
         uint64_t timer_tick, uint32_t frequency);
-static void get_absolute_time(struct soft_timer_timeval *absolute_time,
-        uint32_t timer_tick, uint32_t timer_secs) __attribute__ ((unused));  // before removing
 static uint64_t get_extended_time(uint32_t timer_tick, uint64_t timer_tick_64);
 
 
@@ -54,57 +52,6 @@ void iotlab_time_extend_relative(struct soft_timer_timeval *time,
     iotlab_time_convert(time, timer_tick_64);
 }
 
-
-
-/*
- * Get the absolute time using a 'ticks' timer, and a 'seconds' timer
- *
- * The 'seconds' timer should be have a value taken after the 'ticks' timer.
- * The function supports timer_secs to be taken 0xF seconds after measuring timer_tick.
- *
- */
-static void get_absolute_time(struct soft_timer_timeval *absolute_time,
-        uint32_t timer_tick, uint32_t timer_secs)
-{
-    uint32_t seconds_s     = timer_secs;
-    uint32_t seconds_ticks = get_seconds(timer_tick, SOFT_TIMER_FREQUENCY_FIX);
-
-    uint32_t final_secs;
-
-
-    /*
-     *
-     * Get the small variations from the timer_ticks calculation of seconds
-     * And the big part of the value from the timer_seconds
-     *
-     * In the case where the fact that timer_seconds is taken later,
-     * makes its 4 LSB bytes "overflow", 0x10 should be removed
-     * It's detected comparing the 5th LSB of both timers calculation of seconds.
-     *
-     * Example:
-     * seconds_ticks = 0x0F0A
-     * seconds_s = 0x0F0C (taken 2 seconds later)
-     * final_secs = 0x0F00 | 0x000A == 0x0F00A
-     * final_secs -= 0  // everything fine
-     * final_secs == 0x0F0A == seconds_ticks (which is right for small values)
-     *
-     * seconds_ticks = 0x0FFE
-     * seconds_s = 0x1000 (taken 2 seconds later) // here, 0x10 seconds should be removed
-     * final_secs = 0x1000 | 0x000E == 0x100E
-     * final_secs -= 0x10
-     * final_secs = 0x0FFE == seconds_ticks (whics is what is expected for small values)
-     *
-     */
-
-    // Works because 2**32 is a multiple of 32768
-    // Get the big part from the seconds, and the small from the ticks.
-    final_secs = (seconds_s & (~ 0xF)) | (seconds_ticks & (0xF));
-    // remove the 'overflow' if necessary
-    final_secs -= (seconds_s & (0x10)) ^ (seconds_ticks & (0x10));
-
-    absolute_time->tv_sec = final_secs;
-    absolute_time->tv_usec = get_microseconds(timer_tick, SOFT_TIMER_FREQUENCY_FIX);
-}
 
 
 /*
